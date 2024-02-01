77022cab6ecafee57e1afddaa38695c7cad48419
Lance Hart <shipsimfan@hotmail.com>
2024-01-31 20:28:21 -0500

    Added timer_delete

--- src/time/mod.rs
+++ src/time/mod.rs
@@ -6,6 +6,6 @@ mod structures;
 mod types;
 
 pub use constants::*;
-pub use functions::{timer_create, timer_gettime, timer_settime};
+pub use functions::{timer_create, timer_delete, timer_gettime, timer_settime};
 pub use structures::{itimerspec, timespec};
 pub use types::{clock_t, clockid_t, time_t, timer_t};
