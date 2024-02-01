d5c128f3465b5ae11812799b51509b55416ad3ec
Lance Hart <shipsimfan@hotmail.com>
2024-01-31 20:33:15 -0500

    Added timer_getoverrun

--- src/time/functions/mod.rs
+++ src/time/functions/mod.rs
@@ -1,9 +1,11 @@
 mod timer_create;
 mod timer_delete;
+mod timer_getoverrun;
 mod timer_gettime;
 mod timer_settime;
 
 pub use timer_create::timer_create;
 pub use timer_delete::timer_delete;
+pub use timer_getoverrun::timer_getoverrun;
 pub use timer_gettime::timer_gettime;
 pub use timer_settime::timer_settime;
