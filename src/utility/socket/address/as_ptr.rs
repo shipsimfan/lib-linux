41820e2ee319f0a5ee87d00b87b9f44b6fb82a34
Lance Hart <me@lancehart.ca>
2025-03-09 12:07:20 -0400

    Added as_mut_ptr to SocketAddress and from_raw to Socket

--- src/utility/socket/address/as_ptr.rs
+++ src/utility/socket/address/as_ptr.rs
@@ -8,4 +8,12 @@ impl SocketAddress {
             SocketAddress::V6(addr) => addr as *const _ as _,
         }
     }
+
+    /// Gets a mutable pointer to the underlying socket address
+    pub fn as_mut_ptr(&mut self) -> *mut sockaddr {
+        match self {
+            SocketAddress::V4(addr) => addr as *mut _ as _,
+            SocketAddress::V6(addr) => addr as *mut _ as _,
+        }
+    }
 }
--- src/utility/socket/new.rs
+++ src/utility/socket/new.rs
@@ -6,4 +6,9 @@ impl Socket {
     pub fn new(domain: c_int, r#type: c_int, protocol: c_int) -> Result<Self> {
         try_linux!(socket(domain, r#type, protocol)).map(|handle| Socket { handle })
     }
+
+    /// Creates a new [`Socket`] from a raw `handle`
+    pub unsafe fn from_raw(handle: c_int) -> Socket {
+        Socket { handle }
+    }
 }
