4f9f5741e420a8cd0673c72bc8ace0e13aeba5dc
Lance Hart <lhart@aagcnc.com>
2025-07-14 10:25:25 -0400

    Updated socket interface

--- src/utility/socket/recv.rs
+++ src/utility/socket/recv.rs
@@ -2,8 +2,17 @@ use crate::{sys::socket::recv, try_linux, Result, Socket};
 use std::ffi::c_int;
 
 impl Socket {
-    /// Receive a message from a socket
-    pub fn recv(&mut self, buf: &mut [u8], flags: c_int) -> Result<usize> {
+    /// Read data from a socket
+    ///
+    /// This function should be used on stream sockets
+    pub fn read(&mut self, buf: &mut [u8], flags: c_int) -> Result<usize> {
+        self.recv(buf, flags)
+    }
+
+    /// Read a packet from a socket
+    ///
+    /// This function should be used on datagram and sequential packet sockets
+    pub fn recv(&self, buf: &mut [u8], flags: c_int) -> Result<usize> {
         try_linux!(recv(self.handle, buf.as_mut_ptr().cast(), buf.len(), flags))
             .map(|bytes| bytes as _)
     }
--- src/utility/socket/send.rs
+++ src/utility/socket/send.rs
@@ -2,8 +2,17 @@ use crate::{sys::socket::send, try_linux, Result, Socket};
 use std::ffi::c_int;
 
 impl Socket {
-    /// Send a message on a socket
-    pub fn send(&mut self, buf: &[u8], flags: c_int) -> Result<usize> {
+    /// Write data to a socket
+    ///
+    /// This function should be used on stream sockets
+    pub fn write(&mut self, buf: &[u8], flags: c_int) -> Result<usize> {
+        self.send(buf, flags)
+    }
+
+    /// Send a packet on a socket
+    ///
+    /// This function should be used on datagram and sequential packet sockets
+    pub fn send(&self, buf: &[u8], flags: c_int) -> Result<usize> {
         try_linux!(send(self.handle, buf.as_ptr().cast(), buf.len(), flags)).map(|bytes| bytes as _)
     }
 }
