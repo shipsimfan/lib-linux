c142a4667df35c1ee067e151e25bf22cabfaf432
Lance Hart <lhart@aagcnc.com>
2025-07-21 11:38:52 -0400

    Updated eventfd comment

--- src/utility/eventfd/get.rs
+++ src/utility/eventfd/get.rs
@@ -2,8 +2,8 @@ use crate::EventFd;
 use std::ffi::c_int;
 
 impl EventFd {
-    /// Get the underlying epoll handle
-    pub fn handle(&self) -> c_int {
+    /// Get the underlying event handle
+    pub const fn handle(&self) -> c_int {
         self.handle
     }
 }
