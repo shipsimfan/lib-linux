14c628bee5c1120bd35e11c3de3b018875d87a56
Lance Hart <shipsimfan@hotmail.com>
2026-04-23 15:29:22 -0400

    Fix CPU_ZERO macro

--- src/sched/macros/cpu_zero.rs
+++ src/sched/macros/cpu_zero.rs
@@ -3,7 +3,7 @@
 macro_rules! CPU_ZERO {
     ($set: expr) => {{
         let i_max = std::mem::size_of::<$crate::sched::cpu_set_t>()
-            / std::mem::size_of::<$crate::__cpu_mask>();
+            / std::mem::size_of::<$crate::sched::__cpu_mask>();
         let mut i = 0;
         while i < i_max {
             $set.__bits[i] = 0;
